2cbf6a1d925e1d18e42aa5a5c55b811497407cbb
Matej Vasek <mvasek@redhat.com>
2022-11-30 15:37:40 +0100

    [DEBUG ONLY]
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- test/_oncluster/scenario_runtime_test.go
+++ test/_oncluster/scenario_runtime_test.go
@@ -4,11 +4,16 @@
 package oncluster
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"knative.dev/func/k8s"
 	common "knative.dev/func/test/_common"
 	e2e "knative.dev/func/test/_e2e"
 )
@@ -16,7 +21,6 @@ import (
 // TestRuntime will invoke a language runtime test against (by default) to all runtimes.
 // The Environment Variable E2E_RUNTIMES can be used to select the languages/runtimes to be tested
 func TestRuntime(t *testing.T) {
-
 	var runtimeList = []string{}
 	runtimes, present := os.LookupEnv("E2E_RUNTIMES")
 
@@ -36,6 +40,23 @@ func TestRuntime(t *testing.T) {
 }
 
 func runtimeImpl(t *testing.T, lang string) {
+	start := time.Now()
+	defer func() {
+		cli, err := k8s.NewKubernetesClientset()
+		if err != nil {
+			panic(err)
+		}
+		opt := metav1.ListOptions{}
+		l, err := cli.EventsV1().Events("default").List(context.Background(), opt)
+		if err != nil {
+			panic(err)
+		}
+		for _, item := range l.Items {
+			if item.EventTime.After(start) {
+				_, _ = fmt.Fprintf(os.Stderr, "event: %+v\n", item)
+			}
+		}
+	}()
 
 	var gitProjectName = "test-func-lang-" + lang
 	var gitProjectPath = filepath.Join(t.TempDir(), gitProjectName)
@@ -54,6 +75,7 @@ func runtimeImpl(t *testing.T, lang string) {
 	GitInitialCommitAndPush(t, gitProjectPath, remoteRepo.ExternalCloneURL)
 
 	knFunc.Exec("deploy",
+		"-v",
 		"-r", e2e.GetRegistry(),
 		"-p", funcPath,
 		"--remote",
