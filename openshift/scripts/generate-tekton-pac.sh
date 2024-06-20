ad1bce8b46ed9e0fb651c9c8f18b6605a24f271f
Matej Vašek <matejvasek@gmail.com>
2024-06-20 20:36:14 +0200

    chore: updated branch sync code
    Signed-off-by: Matej Vašek <matejvasek@gmail.com>

--- openshift/scripts/generate-tekton-pac.sh
+++ openshift/scripts/generate-tekton-pac.sh
@@ -6,9 +6,9 @@ FUNC_S2I="pkg/pipelines/resources/tekton/task/func-s2i/0.1/func-s2i"
 FUNC_BUILDPACKS="pkg/pipelines/resources/tekton/task/func-buildpacks/0.2/func-buildpacks"
 FUNC_DEPLOY="pkg/pipelines/resources/tekton/task/func-deploy/0.1/func-deploy"
 
-sed -e 's/^kind: .*/kind: Task/' "${FUNC_S2I}.yaml" > "${FUNC_S2I}-pac.yaml"
-sed -e 's/^kind: .*/kind: Task/' "${FUNC_BUILDPACKS}.yaml" > "${FUNC_BUILDPACKS}-pac.yaml"
-sed -e 's/^kind: .*/kind: Task/' "${FUNC_DEPLOY}.yaml" > "${FUNC_DEPLOY}-pac.yaml"
-
+for fname in "${FUNC_S2I}" "${FUNC_BUILDPACKS}" "${FUNC_DEPLOY}"; do
+    cp "${fname}.yaml" "${fname}-pac.yaml"
+    sed -i 's/^kind: Task$/kind: ClusterTask/g' "${fname}.yaml" 
+done
 
 sed -r -i 's#/func-(\w+)\.yaml#/func-\1-pac.yaml#g' pkg/pipelines/tekton/templates.go
