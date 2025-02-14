1d04e5c2d069fab92537b7efc09359f68db66cef
Chris Suszyński <ksuszyns@redhat.com>
2025-02-14 18:49:46 +0100

    Use hacks directly

--- openshift/scripts/generate-dockerfiles.sh
+++ openshift/scripts/generate-dockerfiles.sh
@@ -7,23 +7,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-function install_generate_hack_tool() {
-  local tmp_dir
-  tmp_dir="$(mktemp -d)"
-
-  git clone --branch main --depth 1 https://github.com/openshift-knative/hack.git "${tmp_dir}"
-  cd "${tmp_dir}" && \
-    go install github.com/openshift-knative/hack/cmd/generate && \
-    cd - && rm -rf "${tmp_dir}"
-
-  return $?
-}
-
 repo_root_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../..
 
-install_generate_hack_tool || exit 1
-
-"$(go env GOPATH)"/bin/generate \
+go run github.com/openshift-knative/hack/cmd/generate@latest \
   --root-dir "${repo_root_dir}" \
   --generators dockerfile \
   --dockerfile-image-builder-fmt "registry.ci.openshift.org/openshift/release:rhel-8-release-golang-%s-openshift-4.19" \
