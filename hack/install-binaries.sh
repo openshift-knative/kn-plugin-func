320b051c525b898f2374ad5f4f9dce9e908a7da5
Matej Vašek <mvasek@redhat.com>
2025-10-05 16:05:22 +0200

    dapr install upgrade
    picking knative/func#3055
    
    Co-authored-by: David Fridrich <fridrich.david19@gmail.com>
    Co-authored-by: Luke Kingland <lkinglan@redhat.com>
    Signed-off-by: Matej Vašek <mvasek@redhat.com>

--- hack/install-binaries.sh
+++ hack/install-binaries.sh
@@ -27,7 +27,7 @@ install_binaries() {
 
   local kubectl_version=1.32.0
   local kind_version=0.26.0
-  local dapr_version=1.11.0
+  local dapr_version=1.16.0
   local helm_version=3.12.0
   local stern_version=1.25.0
   local kn_version=1.13.0
