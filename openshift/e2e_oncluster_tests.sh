cba1b9e0c8569f48cf4e1edf0e04dfedd8bf6f0e
Jefferson Ramos <jrangelramos@gmail.com>
2025-12-09 16:48:56 -0300

    test: clean up ci prow scripts (#1402)

--- openshift/e2e_oncluster_tests.sh
+++ openshift/e2e_oncluster_tests.sh
@@ -28,18 +28,9 @@ export ARTIFACT_DIR="${ARTIFACT_DIR:-$(dirname "$(mktemp -d -u)")/build-${BUILD_
 export ARTIFACTS="${ARTIFACTS:-${ARTIFACT_DIR}}/kn-func/e2e-oncluster-tests"
 mkdir -p "${ARTIFACTS}"
 
-# Prepare Build Parameters
-FUNC_UTILS_IMG="${FUNC_UTILS_IMG:-ghcr.io/knative/func-utils:latest}"
-LDFLAGS="-X knative.dev/func/pkg/k8s.SocatImage=${FUNC_UTILS_IMG}"
-LDFLAGS+=" -X knative.dev/func/pkg/k8s.TarImage=${FUNC_UTILS_IMG}"
-LDFLAGS+=" -X knative.dev/func/pkg/pipelines/tekton.DeployerImage=${FUNC_UTILS_IMG}"
-export GOFLAGS="'-ldflags=${LDFLAGS}'"
-FUNC_REPO_REF="${FUNC_REPO_REF:-openshift-knative/kn-plugin-func}"
-FUNC_REPO_BRANCH_REF="${FUNC_REPO_BRANCH_REF:-release-next}"
-
 # Build 'func' binary
 echo "=== building func binary"
-env FUNC_REPO_REF="${FUNC_REPO_REF}" FUNC_REPO_BRANCH_REF="${FUNC_REPO_BRANCH_REF}" make build
+make build
 
 # Patch Tests for Openshift
 sed -i 's|http://%v.%s.%s|https://%v-%s.%s|' ./e2e/e2e_*.go
@@ -52,7 +43,7 @@ echo "=== running func e2e Remote tests"
 export FUNC_E2E_NAMESPACE="$(oc project -q)"
 export FUNC_E2E_DOMAIN="$(oc get ingresses.config cluster -o jsonpath='{.spec.domain}')"
 export FUNC_E2E_BIN="$(pwd)/func"
-export FUNC_E2E_CLUSTER_REGISTRY="image-registry.openshift-image-registry.svc:5000/${FUNC_E2E_NAMESPACE}"
+export FUNC_E2E_CLUSTER_REGISTRY="${FUNC_E2E_CLUSTER_REGISTRY:-ttl.sh/knfuncci$(head -c 128 </dev/urandom | LC_CTYPE=C tr -dc 'a-z0-9' | head -c 6)}"
 export FUNC_E2E_KUBECONFIG="$KUBECONFIG"
 
 go_test_e2e -v -timeout 15m -tags="e2e" -run TestRemote_Deploy ./e2e || fail_test 'kn-func e2e tests'
